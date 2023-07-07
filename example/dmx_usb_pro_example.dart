a986ee2e548f2a1a37137aa4aac74958974bc2d0
Takashi Kawasaki <espresso3389@gmail.com>
2023-07-07 10:48:48 +0900

    Update the things.

--- example/dmx_usb_pro_example.dart
+++ example/dmx_usb_pro_example.dart
@@ -10,13 +10,13 @@ void main() {
   dmxUsbPro.setTimeouts(10, 100);
   dmxUsbPro.purge(DmxUsbPro.RX);
 
+  // just a test for my purpose
   final buf = Uint8List(75);
   buf[0] = 0;
   for (int i = 0; i < 10000; i++) {
     buf[1] = i;
     buf[2] = i;
     buf[3] = i;
-    print('Light $i');
     dmxUsbPro.write(EnttecProLabels.SET_DMX_TX_MODE, buf);
   }
 }
